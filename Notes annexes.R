af5eed7de2f130f4a50fd705e486192e8c95ac20
norahmartin <norah.martin@student.umons.ac.be>
2025-11-22 15:42:02 +0000

    Problème de ramage avec la référence, intro, +nouveau doc reference

---
+++ Notes annexes.R
@@ -0,0 +1,4 @@
+Introduction: 
+
+  Examiner si les personnes ont été correctement renvoyées chez elles pour travailler en comparant lieu de travail et lieu de résidence — NB : le lieu de résidence ne concerne pas uniquement les personnes qui travaillent, donc on s’attend forcément à une légère diminution en 2022, mais à une augmentation par rapport à 2020. Peut-être en raison d’un plus fort chômage ?).Nous voulons également vérifier si les gens sont retournés dans les bureaux après le Covid.Enfin, nous cherchons à savoir si le fait de pouvoir rester chez eux a entraîné plus d’activités extérieures (même si ces activités concernent aussi des personnes qui ne travaillent pas).
+
